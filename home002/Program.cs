54b1e149c82248c2e891e05539d54d24f5c1e940
Svutlana <fire_fly_74@mail.ru>
2022-12-15 21:09:27 +0500

    home002

---
+++ home002/Program.cs
@@ -0,0 +1,26 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Summa(int C)
+{
+    int res = 0;
+    while ((C / 10 != 0) || (C % 10 !=0))
+    {
+        res = res + C % 10;
+        C = C / 10;
+    }
+    return Math.Abs(res);
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+// Ввести число
+int number = EnterData("Введите число: ");
+// найти сумму цифр числа
+int summ = Summa(number);
+// вывести результат на экран
+Console.WriteLine(summ);
