4b367cd7a8bf341d7799b8a0ab3f3582576163e3
Svutlana <fire_fly_74@mail.ru>
2022-12-15 21:26:49 +0500

    home003

---
+++ home003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] CreateMassiv(int el)
+{
+    int[] array = new int[el];
+    for (int i = 0; i < el; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+
+// Задать массив из 8 случайн;
+int[] arr = CreateMassiv(8);
+// Вывести массив на экран
+Console.WriteLine(String.Join(", ", arr));
