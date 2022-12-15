d16cbab1e6859a1239f2015d4975e2401d3f7956
Svutlana <fire_fly_74@mail.ru>
2022-12-15 20:33:58 +0500

    home001

---
+++ home001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B (без использования Math.Pow).
+
+int Multi(int X, int Y)
+{
+    int res = 1;
+    for (int count = 0; count < Y; count++)
+    {
+        res = res * X;
+    }
+    return res;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+// ввести число А
+int A = EnterData("Введите число А: ");
+// ввести число В
+int B = EnterData("Введите число B: ");
+// возвести число А в степень В
+int C = Multi(A, B);
+// вывести результат на экран
+Console.WriteLine(C);
